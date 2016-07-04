8c768b986a5d2498f6d0a5915a7e2f70dc3922d3
ItsASine <ItsASine@users.noreply.github.com>
2016-07-04 19:10:28 -0400

    Add problem 3 solution :grimacing:

---
+++ problem-003/problem-003.js
@@ -0,0 +1,22 @@
+/*
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+ What is the largest prime factor of the number 600851475143 ?
+ */
+
+var euler3 = function(){
+    var number = 600851475143;
+    var prime = 2;
+
+    while(prime <= number) {
+        if(number % prime == 0) {
+            number = number/prime;
+        } else {
+            prime++;
+        }
+    }
+
+    console.log(prime);
+};
+
+euler3();
